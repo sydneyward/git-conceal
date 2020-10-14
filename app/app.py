ee15b68f78266c1d29eb3c0d0f0368d0c383057d
Sydney Ward <saw170030@utdallas.edu>
2020-10-13 21:38:40 -0500

    Add pre-commit linter

---
+++ app/app.py
@@ -0,0 +1,8 @@
+"""Change me"""
+def main():
+  """Change me too
+  """
+  print("Halppppp")
+
+if __name__ == "__main__":
+  main()
---
+++ scripts/install-requirements.sh
@@ -0,0 +1,9 @@
+path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+cd $path
+
+hooks=$( ls hooks | tr " " "\n")
+for hook in $hooks
+do 
+  chmod +x hooks/$hook
+  ln -f hooks/$hook ../.git/hooks/$hook
+done 
